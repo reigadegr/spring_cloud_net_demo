6af178123811cb05557af71f3d72f82b8a919edc
reigadegr <2722688642@qq.com>
2024-05-14 13:24:57 +0800

    完善测试类

---
+++ stream_consumer/src/main/java/com/huike/stream/ProducerTest.java
@@ -0,0 +1,23 @@
+package com.huike.stream;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import com.huike.stream.Producer.MessageSender;
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest
+//@SpringBootApplication
+//public class ProducerTest {
+//    @Autowired
+//    private MessageSender messageSender;
+//    @Test
+//    public void testSend() {
+//        messageSender.send("hello world");
+///* for (int i = 0; i <5 ; i++) {
+//messageSender.send("1");
+//}*/
+//    }
+//}
